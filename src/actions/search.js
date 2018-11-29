7b1e04f1e9121dd0ecc7263b4ab110a9db424487
sswingle <seiji.swingle@gmail.com>
2018-11-28 19:48:13 -0800

    Completed reducers

--- src/actions/search.js
+++ src/actions/search.js
@@ -2,18 +2,16 @@ import searchYouTube from '../lib/searchYouTube.js';
 import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+
 
 
 var handleVideoSearch = (q) => {
   //TODO:  Write an asynchronous action to handle a video search!
-  const store = createStore(reducer, applyMiddleware(thunk));
-  
-  store.dispatch({
-    key: YOUTUBE_API_KEY,
-    query: q
-  })
+
+  // store.dispatch({
+  //   key: YOUTUBE_API_KEY,
+  //   query: q
+  // })
 
   //   return {
   //     type: "CHANGE_VIDEO_LIST",
@@ -22,13 +20,17 @@ var handleVideoSearch = (q) => {
 
   
 
-  // var search = {
-  //   key: YOUTUBE_API_KEY,
-  //   query: q
-  // }
-  // return (dispatch) => {
-  //   searchYouTube(search, callback)
-  // }
+  const search = {
+    key: YOUTUBE_API_KEY,
+    query: q
+  }
+  return (dispatch) => {
+    searchYouTube(search,(items)=> {
+      dispatch(changeVideo(items[0]))
+      dispatch(changeVideoList(items))
+    }
+  )
+  }
 };
 
 export default handleVideoSearch;
--- src/store/store.js
+++ src/store/store.js
@@ -3,3 +3,11 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+
+export default function store(state) {
+    return createStore(
+        rootReducer,
+        state,
+        applyMiddleware(thunk)
+    );
+}
